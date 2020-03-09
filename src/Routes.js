bd75e7f58ed7d23373653ec50ee4e92856acd3ce
YugmaP <40856628+YugmaP@users.noreply.github.com>
2020-03-09 23:26:44 +0530

    Menu routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Layout from './core/Layout';
 import Home from './core/Home';
+import Menu from './core/Menu';
 
 const Routes = () => {
     return (
         <BrowserRouter>
+            <Menu/>
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/layout" exact component={Layout} />
