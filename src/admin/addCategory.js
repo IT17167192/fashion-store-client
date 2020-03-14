c8e61c673aaaca32994054384d04a40ae7972f2b
YugmaP <40856628+YugmaP@users.noreply.github.com>
2020-03-14 18:49:17 +0530

    AddCategory and mdbootstrap attachment to a component Category table added

--- src/admin/addCategory.js
+++ src/admin/addCategory.js
@@ -1,13 +1,35 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Layout from "../core/Layout";
 import {isAuthenticate} from "../auth";
 import { Link } from "react-router-dom";
 import "mdbreact/dist/css/mdb.css";
+import {createCategory} from "./apiAdmin";
+import { MDBContainer, MDBAlert } from 'mdbreact';
+import { MDBDataTable } from 'mdbreact';
+import {getAllCategories} from "../core/apiCore";
 
 const AddCategory = () => {
     const [name, setName] = useState('');
     const [error, setError] = useState(false);
     const [success, setSuccess] = useState(false);
+    const [loader, setLoader] = useState(false);
+    const [categories, setCategories] = useState([]);
+    const [errorCat, setErrorCat] = useState(false);
+
+    const loadCategories = () => {
+        getAllCategories().then(data => {
+            if (data.error) {
+                setErrorCat(data.error);
+                console.error(errorCat);
+            } else {
+                setCategories(data);
+            }
+        })
+    };
+
+    useEffect(() => {
+        loadCategories();
+    }, []);
 
     //get user and info from local storage
     const {user, token} = isAuthenticate();
@@ -20,11 +42,95 @@ const AddCategory = () => {
 
     const submit = (event) => {
         event.preventDefault();
+        setLoader(true);
         setError(false);
         setSuccess(false);
         //create category
         //use api request
+        createCategory(user._id, token, {name})
+            .then(data=>{
+                setLoader(false);
+                if(data.error){
+                    setError(true);
+                    setSuccess(false);
+                }else{
+                    setError(false);
+                    setSuccess(true);
+                    loadCategories();
+                }
+            })
 
+    };
+
+    const showSuccessMsg = () => {
+        if(success){
+            return(
+                <MDBContainer>
+                    <MDBAlert color="success" dismiss>
+                        <strong>{name}</strong> is Created!
+                    </MDBAlert>
+                </MDBContainer>
+            );
+        }
+    };
+
+    const categoryTable = () => {
+        console.log(categories);
+            if(categories.length > 0){
+                const data = {columns: [
+                        {
+                            label: 'ID',
+                            field: '_id',
+                            sort: 'asc',
+                            width: 200
+                        },
+                        {
+                            label: 'Name',
+                            field: 'name',
+                            sort: 'asc',
+                            width: 270
+                        },
+                        {
+                            label: 'Created At',
+                            field: 'createdAt',
+                            sort: 'asc',
+                            width: 200
+                        },
+                        {
+                            label: 'Updated At',
+                            field: 'updatedAt',
+                            sort: 'asc',
+                            width: 100
+                        }
+                    ],rows: categories};
+
+                return (
+                    <div className="container-fluid col-md-12 col-lg-12 col-sm-12">
+                        <MDBDataTable
+                            striped
+                            bordered
+                            hover
+                            responsive
+                            data={data}
+                        />
+                    </div>
+                );
+
+            }else{
+                return null;
+            }
+    };
+
+    const showErrorMsg = () => {
+        if(error){
+            return(
+                <MDBContainer>
+                    <MDBAlert color="danger" dismiss>
+                        <strong>{name}</strong> should be unique!
+                    </MDBAlert>
+                </MDBContainer>
+            );
+        }
     };
 
     const newCategoryForm = () => (
@@ -32,20 +138,21 @@ const AddCategory = () => {
             <form onSubmit={submit}>
                 <div className="form-group">
                     <label className="text-muted">Category Name</label>
-                    <input type="text" className="form-control" onChange={handleChange} value={name} autoFocus/>
+                    <input type="text" className="form-control" onChange={handleChange} value={name} autoFocus required/>
                 </div>
                 <div className="form-group">
-                    <button className="btn btn-primary">Create Category</button>
+                    <button className="btn btn-primary">{loader ? 'Loading...' : 'Create Category'}</button>
                 </div>
             </form>
         </div>
     );
 
     return (
-        <Layout title="Add new category" description={`Welcome back ${name}!, Add a new category now!`} className="container-fluid">
-            <div className="row">
+        <Layout title="Add new category" description={`Welcome back ${user.name}!, Add a new category now!`} className="container-fluid">
+                {showSuccessMsg()}
+                {showErrorMsg()}
                 {newCategoryForm()}
-            </div>
+                {categoryTable()}
         </Layout>
     );
 };
