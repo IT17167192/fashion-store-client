23944494afc88c7bba643dab8ad12761b2078794
UpekshaGalappaththi <upekshagalappaththi@hotmail.com>
2020-04-24 02:25:37 +0530

    Created ShowWishlistImage.js

---
+++ src/core/ShowWishlistImage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {API} from "../config";
+
+const ShowWishlistImage = ({item, url}) => (
+    <div className="product-img">
+        <img
+            src={`${API}/${url}/image/${item._id}`}
+            className="mb-3"
+            style={{height: "150px", width: "150px"}}
+        />
+    </div>
+);
+
+export default ShowWishlistImage;
