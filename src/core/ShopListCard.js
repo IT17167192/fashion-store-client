55de838411be3b0b559ccb7ff6ec5d06b77ee197
Kasun Gunasekara <mails4kasun@gmail.com>
2020-04-24 17:56:43 +0530

    card cart btn changes

--- src/core/ShopListCard.js
+++ src/core/ShopListCard.js
@@ -1,7 +1,7 @@
-import React, {useState} from "react";
+import React from "react";
 import {Link, Redirect} from 'react-router-dom';
 import ShopListImage from "./ShopListImage";
-import {addItem, updateItem} from "./CartHelper";
+import {addItem} from "./CartHelper";
 import {updateUserCart} from "./apiCore";
 import {isAuthenticate} from "../auth";
 import '../assets/shop_card_assets/bootstrap/css/bootstrap.min.css';
@@ -11,12 +11,10 @@ import '../assets/shop_card_assets/css/Shopping-Grid.css';
 import '../assets/shop_card_assets/css/styles.css';
 
 const ShopListCard = ({
-                        product,
-                        showViewBtn = true,
-                        cartUpdate = false
-                    }) => {
-
-    const [redirect, setRedirect] = useState(false);
+                          product,
+                          showViewBtn = true,
+                          cartUpdate = false
+                      }) => {
 
     const showBtn = showViewBtn => {
         return (
@@ -40,22 +38,16 @@ const ShopListCard = ({
         }
 
         addItem(product, () => {
-            setRedirect(true);
+            console.log("Item Added");
         })
     };
 
     const calculateDiscountedPrice = (product) => {
         let price = product.price;
-        if(product.currency === '$'){
+        if (product.currency === '$') {
             price = product.price * 180;
         }
         return product.currency === '$' ? parseFloat((price - ((price * product.discount) / 100)) / 180).toFixed(2) : parseFloat(price - ((price * product.discount) / 100)).toFixed(2);
-    };
-
-    const makeRedirect = redirect => {
-        if (redirect) {
-            return <Redirect to="/cart"/>
-        }
     };
 
     const showStock = (quantity) => {
@@ -66,42 +58,51 @@ const ShopListCard = ({
         );
     };
 
+    const showCartBtn = (quantity) => {
+        return quantity > 0 ? (
+            <li><a href="" onClick={addToCart} className="fa fa-shopping-cart"> </a></li>
+        ) : (
+            ''
+        );
+    };
+
     return (
         <div className="product-grid7">
             <div className="product-image7">
-                {makeRedirect(redirect)}
                 <Link to={`/product/${product._id}`} className="mr-2">
                     <ShopListImage item={product} url="product"/>
                 </Link>
                 <ul className="social">
-                    {makeRedirect(redirect)}
                     <Link to={`/product/${product._id}`}>
-                        <li><a href="" className="fa fa-search"></a></li>
+                        <li><a href="" className="fa fa-search"> </a></li>
                     </Link>
                     <li>
-                        <a href="javascript : ;" className="fa fa-shopping-bag"></a>
+                        <a href="javascript : ;" className="fa fa-shopping-bag"> </a>
                     </li>
-                    <li><a href=""  onClick={addToCart} className="fa fa-shopping-cart"></a></li>
+                    {showCartBtn(product.quantity)}
                 </ul>
                 {showStock(product.quantity)}
             </div>
             <div className="product-content">
                 <Link to={`/product/${product._id}`} className="mr-2">
-                    <span style={{'fontSize' : 'x-large'}} className="title"><a href="javascript : ;">{product.name}</a></span>
+                    <span style={{'fontSize': 'x-large'}} className="title"><a href="javascript : ;">{product.name}</a></span>
                 </Link>
                 <br/>
                 <Link to={`/product/${product._id}`} className="mr-2">
-                    <span className="title"><a href="javascript : ;">Category: {product.category && product.category.name}</a></span>
+                    <span className="title"><a
+                        href="javascript : ;">Category: {product.category && product.category.name}</a></span>
                 </Link>
                 <ul className="rating">
-                    <li className="fa fa-star"></li>
-                    <li className="fa fa-star"></li>
-                    <li className="fa fa-star"></li>
-                    <li className="fa fa-star"></li>
-                    <li className="fa fa-star"></li>
+                    <li className="fa fa-star"> </li>
+                    <li className="fa fa-star"> </li>
+                    <li className="fa fa-star"> </li>
+                    <li className="fa fa-star"> </li>
+                    <li className="fa fa-star"> </li>
                 </ul>
-                <div className="price">{product.discount > 0 ? product.currency + ' ' + calculateDiscountedPrice(product) : product.currency + ' ' + parseFloat(product.price).toFixed(2)}
-                    <span style={{"color" : "red"}}>{product.discount > 0 ? product.currency + ' ' +  parseFloat(product.price).toFixed(2) : ''}</span>
+                <div
+                    className="price">{product.discount > 0 ? product.currency + ' ' + calculateDiscountedPrice(product) : product.currency + ' ' + parseFloat(product.price).toFixed(2)}
+                    <span
+                        style={{"color": "red"}}>{product.discount > 0 ? product.currency + ' ' + parseFloat(product.price).toFixed(2) : ''}</span>
                     {console.log(product)}
                 </div>
             </div>
