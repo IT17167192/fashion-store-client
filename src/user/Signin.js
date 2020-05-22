a8b22aaa200a24776284870cf57ced1c795490b8
Kasun Gunasekara <mails4kasun@gmail.com>
2020-05-22 08:30:48 +0530

    sign in form change

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,16 +1,73 @@
-import React, {useEffect, useState} from "react";
-import Layout from "../core/Layout";
-import {Link, Redirect} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+import {Redirect} from "react-router-dom";
 import {signin, authenticate, isAuthenticate} from "../auth";
 import {addItem, getCartProductId, showCart} from "../core/CartHelper";
 import {updateUserCart} from "../core/apiCore";
 import {read} from "./apiUser";
 
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                QuarantineFashion.com
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: '100vh',
+    },
+    image: {
+        backgroundImage: 'url(http://la-madleine.de/wp-content/uploads/2019/07/Luxury-Shopping-1520x1024.jpg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor:
+            theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
 const Signin = () => {
+    const classes = useStyles();
 
     const [values, setValues] = useState({
-        email: 'mails444@gmail.com',
-        password: '1234kasun',
+        email: '',
+        password: '',
         error: '',
         loading: false,
         redirect: false
@@ -75,38 +132,36 @@ const Signin = () => {
         event.preventDefault();
         setValues({...values, error: false, loading: true});
 
-        signin({email, password})
-            .then(data => {
-                if (data.error) {
-                    setValues({...values, error: data.error, loading: false})
-                } else {
-                    authenticate(data, () => {
-                            setValues({...values, redirect: true})
-                        }
-                    );
+        if (email === '' || password === '') {
+            setValues({...values, error: 'Fields cannot be empty', loading: false})
+        } else {
+            signin({email, password})
+                .then(data => {
+                    if (data.error) {
+                        setValues({...values, error: data.error, loading: false})
+                    } else {
+                        authenticate(data, () => {
+                                setValues({...values, redirect: true})
+                            }
+                        );
 
-                    //get cart items one by one and add to db
-                    if (items.length > 0) {
-                        for (let i = 0; i < items.length; i++) {
-                            setProduct(items[i]);
+                        //get cart items one by one and add to db
+                        if (items.length > 0) {
+                            for (let i = 0; i < items.length; i++) {
+                                setProduct(items[i]);
+                            }
                         }
+
+                        setDbProductsToCart();  //set db items to cart
                     }
-
-                    setDbProductsToCart();  //set db items to cart
-                }
-            })
+                })
+        }
     };
 
     const showError = () => (
         <div className="alert alert-danger text-center" style={{display: error ? '' : 'none'}}>
             {error}
         </div>
-    );
-
-    const showLoading = () => (
-        loading && (<div className="alert alert-info">
-            <h2>Loading...</h2>
-        </div>)
     );
 
     const redirectUser = () => {
@@ -123,42 +178,74 @@ const Signin = () => {
         }
     };
 
-    const signInForm = () => (
-
-        <div className="container d-flex mt-5 justify-content-center">
-            <div className="col-sm-5">
-                <h3 className="text-center mt-4 font-weight-bolder">Sign in</h3>
-                <div className="card card-body mt-5">
-                    <form>
-                        <div className="form-group">
-                            <label className="text-muted">Email</label>
-                            <input type="email" onChange={handleChange('email')} className="form-control"
-                                   value={email}/>
-                        </div>
-
-                        <div className="form-group">
-                            <label className="text-muted">Password</label>
-                            <input type="password" onChange={handleChange('password')} className="form-control"
-                                   value={password}/>
-                        </div>
-                        {showLoading()}
+    return (
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline/>
+            <Grid item xs={false} sm={4} md={7} className={classes.image}/>
+            <Grid className="p-5" item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon/>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form className={classes.form} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            onChange={handleChange('email')}
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            onChange={handleChange('password')}
+                            autoComplete="current-password"
+                        />
                         {showError()}
-                        <button className="btn btn-primary w-100" disabled={values.loading}
-                                onClick={clickSubmit}>Sign
-                            in
-                        </button>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            onClick={clickSubmit}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="/signup" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                        <Box mt={5}>
+                            <Copyright/>
+                        </Box>
                     </form>
                 </div>
-            </div>
-        </div>
-    );
-
-    return (
-        <div>
-            {signInForm()}
+            </Grid>
             {redirectUser()}
-        </div>
-    )
-};
+        </Grid>
+    );
+}
 
 export default Signin;
