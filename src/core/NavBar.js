fdf6456ec55007066f2dac80083a90b07b6955fb
YugmaP <40856628+YugmaP@users.noreply.github.com>
2020-05-24 22:01:02 +0530

    Category id hard coding to the category card and navbar

--- src/core/NavBar.js
+++ src/core/NavBar.js
@@ -20,22 +20,22 @@ const NavBar = () => {
             <div className="col-lg-5 text-center mt-lg-5">
                 <div className="row people mt-lg-4">
                     <div className="col-md-4 col-lg-3 item">
-                        <Link to="/product/category/5e6cd92b0befdd2d4caa420cx">
+                        <Link to="/product/category/5e6cd92b0befdd2d4caa420c">
                             <h5 style={fontColor()} className="title text-uppercase">Business Casual</h5>
                         </Link>
                     </div>
                     <div className="col-md-4 col-lg-3 item">
-                        <Link to="/product/category/5e65d07cec753b45bc5021f4">
+                        <Link to="/product/category/5ecaa059be7b48235c6a131f">
                             <h5 style={fontColor()} className="title text-uppercase">Gents Casual</h5>
                         </Link>
                     </div>
                     <div className="col-md-4 col-lg-3 item">
-                        <Link to="/product/category/5e65d07cec753b45bc5021f4">
+                        <Link to="/product/category/5ecaa08abe7b48235c6a1320">
                             <h5 style={fontColor()} className="title text-uppercase">Ladies Casual </h5>
                         </Link>
                     </div>
                     <div className="col-md-4 col-lg-3 item">
-                        <Link to="/product/category/5e65d07cec753b45bc5021f4">
+                        <Link to="/product/category/5ecaa0bbbe7b48235c6a1321">
                             <h5 style={fontColor()} className="title text-uppercase">Ladies Fashion</h5>
                         </Link>
                     </div>
